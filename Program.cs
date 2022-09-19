5cd7ec6baf125568eaba79f100ddf941bbd97631
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-09-19 17:35:57 +0400

    feat: Implementing interface into RESTSpaceXDataServices class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazorAPIClient.DataServices;
 
 namespace BlazorAPIClient
 {
@@ -20,6 +21,9 @@ namespace BlazorAPIClient
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["api_base_url"]) });
+
+            builder.Services.AddHttpClient<ISpaceXDataService, RESTSpaceXDataService>
+                 (spds => spds.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
 
             await builder.Build().RunAsync();
         }
